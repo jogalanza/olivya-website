985e3966eba3758b0eca7dcfd67534a3dc428365
jogalanza <44076387+jogalanza@users.noreply.github.com>
2020-10-30 00:56:51 +0800

    update build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  css: {
-    // Enable CSS source maps.
+  publicPath: process.env.NODE_ENV === 'production' ? '/olivya-website/dist/' : '/',
+  css: {    
     sourceMap: process.env.NODE_ENV !== 'production'
   }
 };
