c0841876193db8fff0c111adc317d8c3698db840
jogalanza <44076387+jogalanza@users.noreply.github.com>
2020-10-30 01:47:19 +0800

    update dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+const path = require('path')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/olivya-website/dist/' : '/',
   css: {    
     sourceMap: process.env.NODE_ENV !== 'production'
   }
-};
+};  
