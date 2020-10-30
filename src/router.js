415cd65e43892decf56ff321993ea797eeda10f4
jogalanza <44076387+jogalanza@users.noreply.github.com>
2020-10-30 09:24:36 +0800

    update dist

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ Vue.use(Router);
 
 export default new Router({
   linkExactActiveClass: 'active',
-  mode: 'hash',
+  mode: 'history',
   routes: [
     {
       path: '/',
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/olivya-website/dist/' : '/',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/demo-site/dist/' : '/',
+  assetsDir: './',
   css: {    
     sourceMap: process.env.NODE_ENV !== 'production'
   }
